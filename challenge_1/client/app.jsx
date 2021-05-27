c757b230444d3d363e9f3d51bcf8f9bb36220571
Melanie DeWitt <melaniebd1986@gmail.com>
2021-05-26 20:38:47 -0600

    setup webpack and index.html

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import $ from 'jquery';
 
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentDidMount() {
+
+  }
+
+
+  render () {
+    return (
+      <div>Hello World</div>
+      );
+  }
+
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
